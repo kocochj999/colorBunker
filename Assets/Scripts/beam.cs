fb1fefa92ed44e4bdb7141148aab037bc567707f
kocochj999 <kocochj999@gmail.com>
2021-01-05 21:36:47 +0700

    add

---
+++ Assets/Scripts/beam.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class beam : MonoBehaviour
+{
+    public GameObject beamObject;
+    public GameObject pie;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Color collisionColor = collision.gameObject.GetComponent<SpriteRenderer>().color;
+        Color pieColor = pie.gameObject.GetComponent<SpriteRenderer>().color;
+        if (collision.tag == "Enemy" && ColorUtility.ToHtmlStringRGB(collisionColor).Equals(ColorUtility.ToHtmlStringRGB(pieColor)))
+        {
+            changeBeamOpacity(1f);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            changeBeamOpacity(0.3f);
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy" && collision.gameObject.GetComponent<SpriteRenderer>().color == pie.gameObject.GetComponent<SpriteRenderer>().color)
+        {
+            changeBeamOpacity(1f);
+            Fire();
+        }
+    }
+    void changeBeamOpacity(float value)
+    {
+        Color tmp = beamObject.GetComponent<SpriteRenderer>().color;
+        tmp.a = value;
+        beamObject.GetComponent<SpriteRenderer>().color = tmp;
+    }
+    void Fire()
+    {
+        Debug.Log("Shoot");
+
+    }
+}
---
+++ Assets/Scripts/spin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Policy;
+using UnityEngine;
+
+public class spin : MonoBehaviour
+{
+    public Joystick joystick;
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = -joystick.Horizontal;
+        float vertical = joystick.Vertical;
+        float rotationZ = Mathf.Atan2(horizontal, vertical) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+    }
+}
