f4b4d0e1e6d7b6f0ac0eab0d439935914f026f04
kocochj999 <kocochj999@gmail.com>
2021-01-06 23:08:48 +0700

    add enmey prefab and spawner

---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy :MonoBehaviour
+{
+   public enum Color
+    {
+        RED,
+        BLUE
+    }
+    public string getColorCode(Color color)
+    {
+        string colorCode;
+        switch (color)
+        {
+            case Color.BLUE:
+                colorCode = "FF0000" ;
+                return colorCode;
+            case Color.RED:
+                colorCode = "2626FF";
+                return colorCode;
+        }
+        return "FFFFFF";
+    }
+}
---
+++ Assets/Scripts/enemyPool.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyPool : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public int poolSize;
+    public GameObject spawn1;
+    public GameObject spawn2;
+    public GameObject spawn3;
+    public GameObject spawn4;
+    public GameObject spawn5;
+    public GameObject spawn6;
+    public GameObject spawn7;
+    public GameObject spawn8;
+    public GameObject initSpawnPos;
+
+    private GameObject[] enemy;
+    private float timeSinceLastSpawn;
+    private float spawnRate = 2f;
+    private int currentEnemy = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeSinceLastSpawn = 0f;
+        enemy = new GameObject[poolSize];
+        for (int i = 0; i < poolSize; i++)
+        {
+            enemy[i] = Instantiate(enemyPrefab, initSpawnPos.transform.position, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        if(timeSinceLastSpawn >= spawnRate && currentEnemy < poolSize)
+        {
+            timeSinceLastSpawn = 0f;
+            enemy[currentEnemy].transform.position = RandomPosition();
+            currentEnemy++;
+        }
+
+    }
+    public Vector3 RandomPosition()
+    {
+        Vector3 spawnPostition = new Vector3(0, 0);
+        int minPos = 1;
+        int maxPos = 9;
+        int randomPos = Random.Range(minPos, maxPos);
+
+
+        switch (randomPos)
+        {
+            case 1:
+                spawnPostition = spawn1.transform.position;
+                break;
+            case 2:
+                spawnPostition = spawn2.transform.position;
+                break;
+            case 3:
+                spawnPostition = spawn3.transform.position;
+                break;
+            case 4:
+                spawnPostition = spawn4.transform.position;
+                break;
+            case 5:
+                spawnPostition = spawn5.transform.position;
+                break;
+            case 6:
+                spawnPostition = spawn6.transform.position;
+                break;
+            case 7:
+                spawnPostition = spawn7.transform.position;
+                break;
+            case 8:
+                spawnPostition = spawn8.transform.position;
+                break;
+        }
+        return spawnPostition;
+    }
+    /*public Color RandomColor()
+    {
+        return ColorUtility.;
+    }*/
+}
