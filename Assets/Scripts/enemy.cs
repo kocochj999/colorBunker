ddb13c374d8addd8cea6edeb1556b6ca739f9f95
kocochj999 <kocochj999@gmail.com>
2021-01-08 14:08:35 +0700

    update enemy chase

--- Assets/Scripts/enemy.cs
+++ Assets/Scripts/enemy.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class enemy :MonoBehaviour
 {
-   public enum Color
+    public Transform target;
+    private float moveSpeed = 1f;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+    private void Update()
+    {
+        Chase(target, moveSpeed);
+    }
+    public enum Color
     {
         RED,
         BLUE
@@ -22,5 +33,12 @@ public class enemy :MonoBehaviour
                 return colorCode;
         }
         return "FFFFFF";
+    }
+    public void Chase(Transform target, float moveSpeed)
+    {
+        Vector3 difference = target.transform.position - transform.position;
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+        transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
     }
 }
